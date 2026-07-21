f327b1b4c6cf40d98bc4e04d4b94e2fbd60dddd7
Scott Forbes <flakefrost@gmail.com>
2026-07-22 09:27:58 +0930

    Add files via upload

--- netlify/functions/get-status.js
+++ netlify/functions/get-status.js
@@ -1,10 +1,20 @@
 const { loadState, expireOldClaims } = require("./_state");
 
+const CACHE_TTL_MS = 8000;
+let cachedResponse = null;
+let cachedAt = 0;
+
 exports.handler = async function (event) {
   try {
+    const now = Date.now();
+    if (cachedResponse && now - cachedAt < CACHE_TTL_MS) {
+      return cachedResponse;
+    }
+
     const { data } = await loadState();
     expireOldClaims(data);
-    return {
+
+    const response = {
       statusCode: 200,
       headers: { "Content-Type": "application/json", "Cache-Control": "no-store" },
       body: JSON.stringify({
@@ -17,6 +27,10 @@ exports.handler = async function (event) {
         stats: data.stats
       })
     };
+
+    cachedResponse = response;
+    cachedAt = now;
+    return response;
   } catch (error) {
     return { statusCode: 500, body: JSON.stringify({ error: String(error.message || error) }) };
   }
